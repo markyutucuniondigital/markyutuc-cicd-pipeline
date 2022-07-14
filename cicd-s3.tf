f20f5c28680d6449672011012f8192f2699d26ef
Mark Yutuc <markyutuc@iamuser.com>
2022-07-14 16:21:20 +0800

    Test

--- cicd-s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "codepipeline_artifacts" {
-  bucket = "pipeline-artifacts-markyutuc"
-} 
-
-resource "aws_s3_bucket_acl" "acl" {
-  bucket = aws_s3_bucket.codepipeline_artifacts.id
-  acl    = "private"
-}
